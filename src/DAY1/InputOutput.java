b4c934c1e1ad2772ff122865d5e7b9fe3b4aa5ba
Manoj <manojcs6317@gmail.com>
2025-11-30 14:17:54 +0530

    remote status check

--- src/DAY1/InputOutput.java
+++ src/DAY1/InputOutput.java
@@ -2,11 +2,11 @@ package DAY1;
 import java.util.Scanner;
 public class InputOutput {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the name :");
         String name = sc.nextLine();
         System.out.print("Enter the age:");
         int age = sc.nextInt();
-        System.out.println("Hello! "+name+",you are "+age+" old.");
+        System.out.println("Hello! " + name + ",you are " + age + " old.");
     }
 }
