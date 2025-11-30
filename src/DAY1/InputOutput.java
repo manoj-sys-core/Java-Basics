13c4edc3d986fe7b431273d6234acb57ebcd64d1
Manoj <manojcs6317@gmail.com>
2025-11-30 14:14:44 +0530

    completed day JRE/JDK/JVM input/output Scanner

---
+++ src/DAY1/InputOutput.java
@@ -0,0 +1,12 @@
+package DAY1;
+import java.util.Scanner;
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the name :");
+        String name = sc.nextLine();
+        System.out.print("Enter the age:");
+        int age = sc.nextInt();
+        System.out.println("Hello! "+name+",you are "+age+" old.");
+    }
+}
