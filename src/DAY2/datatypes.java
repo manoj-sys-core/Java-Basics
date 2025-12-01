511e5b0b00ef7920929b2248718cd27657eca0e2
Manoj <manojcs6317@gmail.com>
2025-12-01 21:55:06 +0530

    completed day 2

---
+++ src/DAY2/datatypes.java
@@ -0,0 +1,26 @@
+package DAY2;
+
+public class datatypes {
+    public static void main(String[] args) {
+//        primitive data types
+        int a = 10;
+        short b = 1;
+        long c = 748934678;
+        float d = 3;
+        double e =3.542;
+        char f = 'f';
+        boolean g = true;
+        System.out.println(d);
+//        Explicit Casting & Implicit casting]
+        int num = 1;
+        double y = num;
+        System.out.println(y);
+        double x =3.452;
+        int z = (int) x;
+        System.out.println(z);
+//        final keyword
+        final double pi = 3.14;
+        System.out.println(pi);
+    }
+
+}
